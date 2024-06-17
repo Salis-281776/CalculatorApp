ae9971259039deabdd87d1d8ef89753a30a5a3a5
Salis-281776 <281776@ust.com>
2024-06-17 16:15:41 +0530

    second time code commit

--- Calc.py
+++ Calc.py
@@ -6,10 +6,13 @@ def div(a,b):
     return a/b
 def multiply(a,b):
     return a*b
+def percent (a,b):
+    return (a/100)*b
 def main():
     add(a,b)
     sub(a,b)
     div(a,b)
     multiply(a,b)
+    percent(a,b)
     
     
