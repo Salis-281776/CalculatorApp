1f8deb0bbe0ae3b14ffeba8d992ddb091802d9d7
Salis-281776 <281776@ust.com>
2024-06-17 16:00:41 +0530

    code commited

---
+++ Calc.py
@@ -0,0 +1,15 @@
+def Add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def div(a,b):
+    return a/b
+def multiply(a,b):
+    return a*b
+def main():
+    add(a,b)
+    sub(a,b)
+    div(a,b)
+    multiply(a,b)
+    
+    
